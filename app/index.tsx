3024cbd23483226a792eb6a65ee12eb7f9b2d3ab
mahmoodiftee <mahmoodiftee@gmail.com>
2025-08-06 17:49:14 +0600

    minor changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,21 @@
 import useAuthStore from '@/store/auth.store';
+import useNotificationStore from '@/store/notification.store';
 import { Redirect } from 'expo-router';
 import React, { useEffect } from 'react';
 import { ActivityIndicator, View } from 'react-native';
 
 
 export default function Index() {
-  const { fetchAuthenticatedUser, isLoading } = useAuthStore();
+  const { user, fetchAuthenticatedUser, isLoading } = useAuthStore();
+  const {
+    fetchUnreadCount,
+  } = useNotificationStore();
   useEffect(() => {
     fetchAuthenticatedUser();
-  }, []);
+    if (user?.id) {
+      fetchUnreadCount(user.id);
+    }
+  }, [user?.id, fetchUnreadCount]);
 
   if (isLoading) {
     return (
