a15f39c88cb23fe460cf8aec5c0a2890bae29552
Mahmood Iftee <mahmoodiftee@gmail.com>
2026-03-16 03:07:01 +0600

    fix: align SDK 55 versions and fix reanimated plugin path

--- app-v2/babel.config.js
+++ app-v2/babel.config.js
@@ -7,7 +7,7 @@ module.exports = function (api) {
         ],
         plugins: [
             "@babel/plugin-proposal-optional-chaining",
-            "react-native-reanimated/plugin",
+            "react-native-worklets/plugin",
         ],
     };
 };
